fe513e54f50bf6afda4e5cd96ae23dc65f42d8db
Josh Meek <josh@prefect.io>
2019-02-26 09:43:34 -0500

    Update live print check

--- lv.go
+++ lv.go
@@ -6,22 +6,19 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
 func main() {
-
 	args := os.Args[1:]
 
 	if len(args) == 0 {
 		fmt.Println("Please provide a command...")
 		return
 	}
-
+	storedOutput := ""
 	for {
-		fmt.Print("\033[2J")   // clear
-		fmt.Print("\033[0;0H") // reset to [0][0] position
-
 		cmd := exec.Command(args[0], args[1:]...)
 
 		var out bytes.Buffer
@@ -31,7 +28,14 @@ func main() {
 			log.Fatal(err)
 		}
 
+		if strings.Compare(storedOutput, out.String()) == 0 {
+			continue
+		}
+
+		fmt.Print("\033[2J")   // clear
+		fmt.Print("\033[0;0H") // reset to [0][0] position
 		fmt.Printf("%s", out.String())
+		storedOutput = out.String()
 
 		time.Sleep(2 * time.Second)
 	}
